1bf001318f0248195ce48736295b33783bfc780b
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-01-29 17:58:52 +0700

    Remove App

--- app/App.jsx
+++ app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header/header.component';
 import AppRouter from './components/app-router/app-router.component';
 import Footer from './components/footer/footer.component';
+import './app.scss';
 
 class App extends React.Component {
     render() {
